af5f465b36f45fa1061b5aa2ae1a695d7a4ebc50
ram3917 <ramashwin.kumar@live.com>
2020-07-14 22:03:40 +0200

    Remove build

--- build/thirdparty/googletest/googlemock/gtest/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/rama/Documents/AsteroidGameCpp/thirdparty/googletest/googletest
-# Build directory: /home/rama/Documents/AsteroidGameCpp/build/thirdparty/googletest/googlemock/gtest
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
